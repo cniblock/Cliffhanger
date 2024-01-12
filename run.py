60b9739f1e51ae269950cb5ff369c692c6c49eab
cniblock <>
2024-01-12 21:42:27 +0000

    added while loop to check guess hasn't already been guessed

--- run.py
+++ run.py
@@ -67,8 +67,13 @@ while incorrect_guesses < incorrect_guesses_allowed:
         print("{}, ".format(letter), end='')
     print()
     print("Guesses remaining: {}".format(incorrect_guesses_allowed - incorrect_guesses))
-    letter_user = input("Enter a letter: ")
+    letter_user = input
+        ("Enter a letter: ")
     if len(letter_user) == 1 and letter_user.isalpha():
         break
     else:
-        print("Error. Please enter a letter.")
+        print("Error. Please enter a l
+    while letter_user in letters_guessed or letter_user in wrong_letters:
+        print()
+        print("You have already guessed this letter. Please guess another letter")
+        letter_user = input("Enter a letter: ")
