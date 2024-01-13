b0cf8fa46edc3e90977e59b8332b0e5c776e2d1f
cniblock <>
2024-01-13 15:05:19 +0000

    added play again input and loop to reset game

--- run.py
+++ run.py
@@ -68,26 +68,26 @@ wrong_letters = []
 
 # Initial print at the start of the game
 print()
-print("Welcome to the Cliffhanger word guessing game\n")
-print("The word has {} letters\n".format(len(letters_word)))
+print( "Welcome to the Cliffhanger word guessing game\n")
+print(" The word has {} letters\n".format(len(letters_word)))
 
 # While loop to check guesses / wrong guesses / ask for user input
 while incorrect_guesses < incorrect_guesses_allowed:
     print()
-    print("Incorrect letters guessed: ", end='')
+    print(" Incorrect letters guessed: ", end='')
     for letter in wrong_letters:
         print("{}, ".format(letter), end='')
     print()
-    print("Guesses remaining: {}".format(incorrect_guesses_allowed - incorrect_guesses))
-    letter_user = input("Enter a letter: ")
+    print(" Guesses remaining: {}".format(incorrect_guesses_allowed - incorrect_guesses))
+    letter_user = input(" Enter a letter: ")
 
     # Validate user input
     if len(letter_user) == 1 and letter_user.isalpha():
         # Check if the letter has already been guessed
         while letter_user in letters_guessed or letter_user in wrong_letters:
             print()
-            print("You have already guessed this letter. Please guess another letter.")
-            letter_user = input("Enter a letter: ")
+            print(" You have already guessed this letter. Please guess another letter.")
+            letter_user = input(" Enter a letter: ")
 
         # Check if the guessed letter is incorrect
         if letter_user not in letters_word:
@@ -98,7 +98,7 @@ while incorrect_guesses < incorrect_guesses_allowed:
         # letters_guessed.append(letter_user)
 
         print()
-        print("Word: ", end="")
+        print(" Word: ", end="")
 
         # For loop to add letter to guesses and check if letter is in word
         for letter in letters_word:
@@ -121,10 +121,24 @@ while incorrect_guesses < incorrect_guesses_allowed:
         # Print game win
         if len(letters_guessed) == len(letters_word):
             print()
-            print("Congratulations! You won!")
+            print(" Congratulations! You won!")
             break
 # Print loss of game
 if incorrect_guesses == incorrect_guesses_allowed:
     print()
-    print("You lost! Better luck next time!")
-    print("The correct word was: {}".format(word))
+    print(" You lost! Better luck next time!")
+    print(" The correct word was: {} \n".format(word))
+    play_again = input("Do you want to play again? (y/n): ").lower()
+
+    if play_again == 'y':
+        # Reset game variables and start a new game
+        word = get_word()
+        incorrect_guesses = 0
+        letters_guessed = []
+        wrong_letters = []
+
+        print("\nWelcome to the Cliffhanger word guessing game\n")
+        print("The word has {} letters\n".format(len(word)))
+    else:
+        print("Thanks for playing. Goodbye!")
+        
